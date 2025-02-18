6480fe832aa68f62d086d5c325e6e9a960628482
MuminovMuslimbek <muslimbekmuminov47@gmail.com>
2025-02-18 22:32:58 +0300

    settings done

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,52 @@
-import React, { createContext, useEffect, useState } from 'react'
-import { Route, Routes, useNavigate } from 'react-router-dom'
-import Home from './pages/Home'
-import Detail from './pages/Detail'
-import Login from './pages/Login'
-import MainLayout from './layout/MainLayout'
-import Settings from './pages/Settings'
-import Users from './pages/Users'
+import React, { createContext, useEffect, useState } from "react";
+import { Route, Routes, useNavigate, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import Login from "./pages/Login";
+import MainLayout from "./layout/MainLayout";
+import Settings from "./pages/Settings";
+import Users from "./pages/Users";
 
-export const LoginUsernameContext = createContext(null)
-export const LoginPasswordContext = createContext(null)
+export const LoginUsernameContext = createContext(null);
+export const LoginPasswordContext = createContext(null);
 
 function App() {
-  const [logUsername, setLogUsername] = useState('admin')
-  const [logPass, setLogPass] = useState('admin')
-  const navigate = useNavigate()
+  const [logUsername, setLogUsername] = useState("admin");
+  const [logPass, setLogPass] = useState("admin");
+  const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
-    if (!localStorage.getItem('UserLogin')) {
-      navigate('/login')
+    if (location.pathname !== "/login") {
+      localStorage.setItem("lastPage", location.pathname);
     }
-  }, [])
+  }, [location]);
+
+  useEffect(() => {
+    const user = localStorage.getItem("UserLogin");
+    if (!user) {
+      navigate("/login");
+    } else {
+      const lastPage = localStorage.getItem("lastPage") || "/";
+      if (location.pathname === "/login") {
+        navigate(lastPage);
+      }
+    }
+  }, []);
 
   return (
     <LoginUsernameContext.Provider value={{ logUsername, setLogUsername }}>
       <LoginPasswordContext.Provider value={{ logPass, setLogPass }}>
         <Routes>
           <Route index element={<MainLayout><Home /></MainLayout>} />
-          <Route path='/:id' element={<MainLayout><Detail /></MainLayout>} />
-          <Route path='/users' element={<MainLayout><Users /></MainLayout>} />
-          <Route path='/settings' element={<MainLayout><Settings /></MainLayout>} />
-          <Route path='/login' element={<Login />} />
+          <Route path="/:id" element={<MainLayout><Detail /></MainLayout>} />
+          <Route path="/users" element={<MainLayout><Users /></MainLayout>} />
+          <Route path="/settings" element={<MainLayout><Settings /></MainLayout>} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </LoginPasswordContext.Provider>
     </LoginUsernameContext.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import Modal from 'react-modal';
 import EyesOpen from '../assets/eyesOpen.svg';
 import EyesClosed from '../assets/eyesClosed.svg';
-import { LoginPasswordContext } from '../App';
+import { LoginPasswordContext, LoginUsernameContext } from '../App';
 
 const customStyles = {
   content: {
@@ -25,7 +25,8 @@ const customStyles = {
 Modal.setAppElement('#root');
 
 function Settings() {
-  const { logPass } = useContext(LoginPasswordContext);
+  const { logUsername, setLogUsername } = useContext(LoginUsernameContext);
+  const { logPass, setLogPass } = useContext(LoginPasswordContext);
   const [type1, setType1] = useState(true);
   const [type2, setType2] = useState(true);
   const [type3, setType3] = useState(true);
@@ -33,26 +34,32 @@ function Settings() {
   const [newPass, setNewPass] = useState('');
   const [conPass, setConPass] = useState('');
   const [modalIsOpen, setIsOpen] = useState(false);
+  const [statusModal, setStatusModal] = useState('username')
 
   function validate() {
+    let ContextVal = statusModal == 'username' ? logUsername : logPass
     if (!oldPass) {
-      alert('Eski parolingizni kiriting!');
+      alert(`Eski ${statusModal}ni kiriting!`);
+      return false;
+    }
+    if (oldPass !== ContextVal) {
+      alert(`Eski ${statusModal}ni notog'ri kiritdingiz!`);
       return false;
     }
     if (!newPass) {
-      alert('Yangi parolingizni kiriting!');
+      alert(`Yangi ${statusModal}ni kiriting!`);
+      return false;
+    }
+    if (newPass == ContextVal) {
+      alert(`Oldingi ${statusModal} bilan birxil iltimos boshqacha ${statusModal} qo\'ying!`);
       return false;
     }
     if (!conPass) {
-      alert('Yangi parolni tastiqlang!');
-      return false;
-    }
-    if (oldPass !== logPass) {
-      alert('Eski parolni notog`ri kiritdingiz!');
+      alert(`Yangi ${statusModal}ni tastiqlang!`);
       return false;
     }
     if (newPass !== conPass) {
-      alert('Parolni notog`ri tastiqladingiz!');
+      alert(`${statusModal}ni notog'ri tastiqladingiz!`);
       return false;
     }
     return true;
@@ -66,8 +73,13 @@ function Settings() {
       return;
     }
 
-    let isChange = confirm('Rostdanham parolni o`zgartirmoqchimisiz?');
+    let isChange = confirm(`Rostdanham ${statusModal}ni o'zgartirmoqchimisiz?`);
     if (isChange) {
+      if (statusModal == 'username') {
+        setLogUsername(setNewPass)
+      } else {
+        setLogPass(setNewPass)
+      }
       setOldPass('');
       setNewPass('');
       setConPass('');
@@ -75,8 +87,9 @@ function Settings() {
     }
   }
 
-  function openModal() {
-    setIsOpen(true);
+  function openModal(arg) {
+    setIsOpen(true)
+    setStatusModal(arg)
   }
 
   function closeModal() {
@@ -86,25 +99,26 @@ function Settings() {
   return (
     <div className="text-[#0C0E16] select-none">
       <div className="flex gap-[7px] text-[12px]">
-        <Link className="hover:underline" to={'/'}> Bosh sahifa </Link>{' '}
+        <Link className="hover:underline" to='/'> Bosh sahifa </Link>{' '}
         <span className="text-gray-400">/</span> <p>Sozlamalar</p>
       </div>
       <h1 className="font-bold text-[#0C0E16] text-[32px]">Sozlamalar</h1>
-      <button onClick={openModal} className="bg-[#FCE000] mt-[10px] px-3 py-2 rounded-md w-full max-w-2xl active:scale-95 transition-[0.3s] cursor-pointer" > Parolni o'zgartirish </button>
-      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles} contentLabel="Parolni o'zgartirish modal">
-        <h2 className="mb-4 font-semibold text-[24px] select-none">Parolni o'zgartirish</h2>
+      <button onClick={() => { openModal('username') }} className="bg-[#FCE000] mt-[40px] px-3 py-2 rounded-md w-full max-w-2xl font-medium active:scale-95 transition-[0.3s] cursor-pointer" > Username o'zgartirish </button>
+      <button onClick={() => { openModal('password') }} className="bg-[#FCE000] mt-[20px] px-3 py-2 rounded-md w-full max-w-2xl font-medium active:scale-95 transition-[0.3s] cursor-pointer" > Parolni o'zgartirish </button>
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles} contentLabel={`${statusModal} o'zgartirish modal`}>
+        <h2 className="mb-4 font-semibold text-[24px] select-none"><span className='capitalize'>{statusModal}</span> o'zgartirish</h2>
         <button onClick={closeModal} className="top-[18px] right-[30px] absolute text-[35px] rotate-[45deg] cursor-pointer select-none" > + </button>
         <form onSubmit={handleSubmit} className="flex flex-col gap-[15px]">
-          <label className="relative flex flex-col gap-[10px]"> Oldingi Parolni kiriting:
-            <input value={oldPass} onChange={(e) => { setOldPass(e.target.value); }} placeholder="Oldingi parol" type={type1 ? 'password' : 'text'} className="bg-white shadow-sm px-3 py-2 border focus:border-[#FCE000] rounded-md outline-none focus:ring-0 w-full min-w-20 max-w-2xl font-medium text-[#0C0E16] placeholder:text-[#0C0E16] text-sm transition-all[0.4s]" />
+          <label className="relative flex flex-col gap-[10px]"> Oldingi {statusModal} kiriting:
+            <input value={oldPass} onChange={(e) => { setOldPass(e.target.value); }} placeholder={`oldingi ${statusModal}`} type={type1 ? 'password' : 'text'} className="bg-white shadow-sm px-3 py-2 border focus:border-[#FCE000] rounded-md outline-none focus:ring-0 w-full min-w-20 max-w-2xl font-medium text-[#0C0E16] placeholder:text-[#0C0E16] text-sm transition-all[0.4s]" />
             <img onClick={() => { setType1(!type1); }} className="right-[14px] bottom-2 z-10 absolute w-[20px] cursor-pointer" src={type1 ? EyesOpen : EyesClosed} />
           </label>
-          <label className="relative flex flex-col gap-[10px]"> Yangi kiritmoqchi bo'lgan paro'lingizni kiriting:
-            <input value={newPass} onChange={(e) => { setNewPass(e.target.value); }} placeholder="Yangi parol" type={type2 ? 'password' : 'text'} className="bg-white shadow-sm px-3 py-2 border focus:border-[#FCE000] rounded-md outline-none focus:ring-0 w-full min-w-20 max-w-2xl font-medium text-[#0C0E16] placeholder:text-[#0C0E16] text-sm transition-all[0.4s]" />
+          <label className="relative flex flex-col gap-[10px]">  Yangi {statusModal}ingizni kiriting:
+            <input value={newPass} onChange={(e) => { setNewPass(e.target.value); }} placeholder={`yangi ${statusModal}`} type={type2 ? 'password' : 'text'} className="bg-white shadow-sm px-3 py-2 border focus:border-[#FCE000] rounded-md outline-none focus:ring-0 w-full min-w-20 max-w-2xl font-medium text-[#0C0E16] placeholder:text-[#0C0E16] text-sm transition-all[0.4s]" />
             <img onClick={() => { setType2(!type2); }} className="right-[14px] bottom-2 z-10 absolute w-[20px] cursor-pointer" src={type2 ? EyesOpen : EyesClosed} />
           </label>
-          <label className="relative flex flex-col gap-[10px]"> Yangi paro'lingizni qayta kiriting:
-            <input value={conPass} onChange={(e) => { setConPass(e.target.value); }} placeholder="Parolni tastiqlash" type={type3 ? 'password' : 'text'} className="bg-white shadow-sm px-3 py-2 border focus:border-[#FCE000] rounded-md outline-none focus:ring-0 w-full min-w-20 max-w-2xl font-medium text-[#0C0E16] placeholder:text-[#0C0E16] text-sm transition-all[0.4s]" />
+          <label className="relative flex flex-col gap-[10px]"> Yangi {statusModal}ingizni tastiqlang:
+            <input value={conPass} onChange={(e) => { setConPass(e.target.value); }} placeholder={`${statusModal}ni tastiqlash`} type={type3 ? 'password' : 'text'} className="bg-white shadow-sm px-3 py-2 border focus:border-[#FCE000] rounded-md outline-none focus:ring-0 w-full min-w-20 max-w-2xl font-medium text-[#0C0E16] placeholder:text-[#0C0E16] text-sm transition-all[0.4s]" />
             <img onClick={() => { setType3(!type3); }} className="right-[14px] bottom-2 z-10 absolute w-[20px] cursor-pointer" src={type3 ? EyesOpen : EyesClosed} />
           </label>
           <button type="submit" className="bg-[#FCE000] mt-[10px] px-3 py-2 rounded-md w-full max-w-2xl active:scale-95 transition-[0.3s] cursor-pointer" > Yangilash </button>
